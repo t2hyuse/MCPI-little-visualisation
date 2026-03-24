5bf7304ec9aa8b460ab33a1c6cd90e796b1afe31
Hyuse <t2hyuse@gmail.com>
2026-03-24 22:24:57 +0100

    first commit

--- main.cpp
+++ main.cpp
@@ -13,45 +13,45 @@ constexpr int HEIGHT = 600;
 constexpr double radius = 1;
 
 double norm(const double x, const double y) {
-    return sqrt(x*x+y*y);
+    return sqrt(x * x + y * y);
 }
 
 void createCircle(int const segments, double const rad, const double x, const double y) {
     glBegin(GL_LINE_LOOP);
-    glColor3d(0.0,1.0,0.0);
+    glColor3d(0.0, 1.0, 0.0);
     for (int i = 0; i < segments; ++i) {
-        glVertex2d(x+rad*std::cos(2*PI*i/segments),y+rad*std::sin(2*PI*i/segments));
+        glVertex2d(x + rad * std::cos(2 * PI * i / segments), y + rad * std::sin(2 * PI * i / segments));
     }
     glEnd();
 }
-using vecPair = std::vector<std::pair<double,double>>;
 
-void addPoints(vecPair &points,double &success, double &total) {
+using vecPair = std::vector<std::pair<double, double> >;
+
+void addPoints(vecPair &points, double &success, double &total) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> dis(0.0, 1.0) ;
+    std::uniform_real_distribution<double> dis(0.0, 1.0);
 
     double x = dis(gen);
     double y = dis(gen);
-    if (norm(x,y) < radius) {
+    if (norm(x, y) < radius) {
         success++;
     }
     total++;
-    points.insert(points.end(),{x,y});
+    points.insert(points.end(), {x, y});
 }
 
 void drawPoints(vecPair const &points) {
-    for (const auto &[fst, snd] : points) {
+    for (const auto &[fst, snd]: points) {
         glBegin(GL_POINTS);
-        glColor3d(1.0,0.0,0.0);
-        glVertex2d(fst,snd);
+        glColor3d(1.0, 0.0, 0.0);
+        glVertex2d(fst, snd);
         glEnd();
     }
 }
 
 
 int main() {
-
     vecPair pointsList;
     double total;
     double success;
@@ -60,12 +60,12 @@ int main() {
         std::cerr << "Failed to initialize GLFW3" << std::endl;
         return 1;
     }
-        GLFWwindow* window{glfwCreateWindow(WIDTH,HEIGHT,"MCPI",nullptr,nullptr)};
-        if (!window) {
-            std::cerr << "Failed to create GLFW3" << std::endl;
-            glfwTerminate();
-            return -1;
-        }
+    GLFWwindow *window{glfwCreateWindow(WIDTH, HEIGHT, "MCPI", nullptr, nullptr)};
+    if (!window) {
+        std::cerr << "Failed to create GLFW3" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
     glfwMakeContextCurrent(window);
     glfwSwapInterval(0);
     int fbWidth{WIDTH}, fbHeight{HEIGHT};
@@ -78,14 +78,14 @@ int main() {
     glLoadIdentity();
 
     while (!glfwWindowShouldClose(window)) {
-        glColor3d(0.0,0.0,0.0);
+        glColor3d(0.0, 0.0, 0.0);
         glClear(GL_COLOR_BUFFER_BIT);
         createCircle(100, 0.5, 0.5, 0.5);
-        addPoints(pointsList,success,total);
+        addPoints(pointsList, success, total);
         drawPoints(pointsList);
         if (static_cast<int>(total) % 5000 == 0 && total != 0) {
             std::cout << std::fixed << std::setprecision(10)
-                      << 4.0 * success / total << std::endl;
+                    << 4.0 * success / total << std::endl;
         }
         if (static_cast<int>(total) % 40000 == 0 && total != 0) {
             pointsList.clear();
@@ -97,6 +97,4 @@ int main() {
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
-
-
 }
